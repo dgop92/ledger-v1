69c9c6313a61b05fb711726de351bca9440d2da6
dgop92 <inevaup@gmail.com>
2026-08-08 11:04:00 -0500

    initial commit

---
+++ src/native-test/java/com/dgop92/ledger_v1/GreetingResourceIT.java
@@ -0,0 +1,8 @@
+package com.dgop92.ledger_v1;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+class GreetingResourceIT extends GreetingResourceTest {
+    // Execute the same tests but in packaged mode.
+}
